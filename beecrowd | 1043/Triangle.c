86155f613303cbfe379ce8477a8bd276a492b88f
Aong Sing Marma <153714740+AongSingMarma23@users.noreply.github.com>
2025-10-22 20:46:47 +0600

    Add files via upload

---
+++ beecrowd | 1043/Triangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+   float a, b, c, Perimetro, Area;
+
+   scanf("%f%f%f", &a, &b, &c);
+
+   if((a + b) > c && (b + c) > a && (a + c) > b){
+
+    Perimetro = a + b + c;
+
+    printf("Perimetro = %.1f", Perimetro);
+   }
+
+   else{
+
+    Area = .5 * (a + b) * c;
+
+    printf("Area = %.1f", Area);
+   }
+
+    return 0;
+}
+
